472602855f9a2cfd85e9c3908c180e683bd4b12b
JoshuaRuiz1 <joshuaruizlopez7@gmail.com>
2023-11-16 14:43:00 -0800

    actualize los canales

--- script.js
+++ script.js
@@ -15,3 +15,44 @@ modal.showModal();
 closeModal.addEventListener("click", () => {
 modal.close();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('searchInput');
+    const channelContainer = document.getElementById('channelContainer');
+    const filterCheckboxes = document.querySelectorAll('.filter-checkbox');
+
+    searchInput.addEventListener('input', function () {
+        updateChannelVisibility();
+    });
+
+    filterCheckboxes.forEach(function (checkbox) {
+        checkbox.addEventListener('change', function () {
+            updateChannelVisibility();
+        });
+    });
+
+    function updateChannelVisibility() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const selectedFilters = Array.from(filterCheckboxes)
+            .filter(checkbox => checkbox.checked)
+            .map(checkbox => checkbox.name);
+
+        const allChannels = document.querySelectorAll('.card-channels');
+
+        allChannels.forEach(function (channel) {
+            const channelTypes = Array.from(channel.querySelectorAll('.channel-types span'))
+                .map(span => span.textContent.trim());
+
+            const channelName = channel.querySelector('h3').textContent.trim().toLowerCase();
+
+            const isVisible = (
+                selectedFilters.every(filter => channelTypes.includes(filter)) &&
+                (channelName.includes(searchTerm) || searchTerm === '')
+            );
+
+            channel.style.display = isVisible ? 'block' : 'none';
+        });
+    }
+});
+
+
